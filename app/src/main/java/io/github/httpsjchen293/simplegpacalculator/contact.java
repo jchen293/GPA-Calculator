0581dad14d34312e9d6ae8fb3112c655b130bb64
jchen293 <jenny0418kobe@gmail.com>
2018-02-21 01:05:22 -0500

    Update contact.java

--- app/src/main/java/io/github/httpsjchen293/simplegpacalculator/contact.java
+++ app/src/main/java/io/github/httpsjchen293/simplegpacalculator/contact.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 
 /**
  * Created by Jack on 8/9/17.
@@ -16,6 +20,10 @@ public class contact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact);
+        MobileAds.initialize(this, "ca-app-pub-7155890232460555~5886927750");
+        AdView adView = (AdView)findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
         back = (Button) findViewById(R.id.back);
 
