a6fce2173d26c8c03e79c62fb7a9bce69b7dad9d
jchen293 <jenny0418kobe@gmail.com>
2018-02-21 01:05:02 -0500

    Update about.java

--- app/src/main/java/io/github/httpsjchen293/simplegpacalculator/about.java
+++ app/src/main/java/io/github/httpsjchen293/simplegpacalculator/about.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 /**
  * Created by Jack on 8/9/17.
@@ -16,8 +19,13 @@ public class about extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        MobileAds.initialize(this, "ca-app-pub-7155890232460555~5886927750");
+        AdView adView = (AdView)findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
-back = (Button) findViewById(R.id.back);
+
+        back = (Button) findViewById(R.id.back);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
